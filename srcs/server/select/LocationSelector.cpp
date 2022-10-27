a30d6f798835dcaf2301d0475a02153d2b784253
Archimbaud <tharchimb@gmail.com>
2022-10-27 14:27:52 +0200

    fixed wildcard in locationSelector, read nice size of char in CGI stdout

--- srcs/server/select/LocationSelector.cpp
+++ srcs/server/select/LocationSelector.cpp
@@ -1,5 +1,5 @@
 #include "LocationSelector.hpp"
-
+bool compareWildCard(const std::string& reference, const std::string& target);
 LocationBlock	LocationSelector::selectLocationBlock(const std::string &path, std::vector<LocationBlock> &serverBlock)
 {
 	std::vector<std::string>	validPaths;
@@ -22,30 +22,55 @@ LocationBlock	LocationSelector::selectLocationBlock(const std::string &path, std
 				maxSizeIndex = i;
 			}
 		}
-		if (path.find(locationPath) != std::string::npos && locationPath.find('*') != std::string::npos)
-		{
-			std::cout << "help " << std::endl;
-			if (locationPath.find('*') == 0)
-			{
-				if (endingWildcard(path, locationPath) == 0)
-					return (serverBlock[i]);
-			}
-			else if (locationPath.find('*') == locationPath.length() - 1)
-			{
-				if (startingWildcard(path, locationPath) == 0)
-					return (serverBlock[i]);
-			}
-		}
+		if (compareWildCard(locationPath, path) == true)
+			return (serverBlock[i]);
 	}
 	return (serverBlock[maxSizeIndex]);
 }
 
-int	LocationSelector::endingWildcard(const std::string &path, std::string locationPath)
+bool compareWildCard(const std::string& reference, const std::string& target)
 {
-	return (path.compare(path.length() - (locationPath.length() - 1), locationPath.length(), locationPath.substr(1)));
-}
+	std::vector<std::string> splittedReference;
+	if (reference.find_first_not_of('*') == std::string::npos) // reference only contains '*'
+		return true;
+	int i = reference.find_first_not_of('*');
+	while (i < static_cast<int>(reference.length()))
+	{
+		std::string str;
+		int end;
+		if (reference.find('*', i) == std::string::npos)
+			end = reference.length();
+		else
+			end = reference.find('*', i);
+		str = reference.substr(i, end - i);
+		if (str.empty() == false) // consecutive wildcards can create empty str
+			splittedReference.push_back(str);
+		i = end + 1;
+	}
 
-int	LocationSelector::startingWildcard(const std::string &path, std::string locationPath)
-{
-	return (path.compare(0, locationPath.length() - 1, locationPath.substr(0, locationPath.length() - 1)));
+	bool startsWithWildcard = reference[0] == '*';
+	bool endsWithWildcard = reference[reference.length() - 1] == '*';
+	if (startsWithWildcard == false)
+	{
+		std::string str = target.substr(0, splittedReference[0].length());
+		if (str != splittedReference[0])
+			return false;
+	}
+	if (endsWithWildcard == false)
+	{
+		const std::string& str = splittedReference[splittedReference.size() - 1];
+		if (target.find(str.c_str(), target.length() - str.length(), str.length()) == std::string::npos)
+			return false;
+	}
+
+	int previousMatch = 0;
+	for (int i = 0; i < (int)splittedReference.size(); i++)
+	{
+		if (target.find(splittedReference[i], previousMatch) != std::string::npos)
+			previousMatch = target.find(splittedReference[i], previousMatch) + splittedReference[i].length();
+		else
+			return false;
+	}
+
+	return true;
 }
