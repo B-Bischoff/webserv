12714171c41d873f8d8730497ccafcbab626ce8c
B-Bischoff <mainbischoffbrice@gmail.com>
2022-10-27 16:30:05 +0200

    added php demo page

---
+++ pages/test2.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+<p>php demo page</p>
+<?php
+	echo 'Hello World!';
+	echo '<hr>';
+
+	$a = 1;
+	$b = 15;
+	$c = $a + $b;
+	echo 'a: ', $a, '<br>';
+	echo 'b: ', $b, '<br>';
+	echo 'a + b: ', $c, '<br>';
+?>
+</body>
+</html> 
